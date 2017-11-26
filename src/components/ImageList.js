41d8b76d6f2397188d1350e8d570576402e7b0b8
Gemma <gemmawvoakes@gmail.com>
2017-11-26 17:57:39 +0000

    Add state to [ImageList] component and pass props to [Image]

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Image from './Image';
+import photoData from '../data/photoData';
 
-function ImageList() {
-  return (
-    <div className='ImageList'>
-      <h1>I am an image list!</h1>
+class ImageList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allImages: photoData.items,
+      faveImages: []
+    }
+  }
+  render() {
+    return (
+      <div className='ImageList'>
+        <div className='container'>
+        <h1>I am an image list!</h1>
         <a className='button is-link is-medium'>Show all</a>
         <a className='button is-link is-medium'>Show favourites</a>
-      <div className='tile is-ancestor'>
-        <Image />
+        {this.state.allImages.length === 0 ?
+          <nav className="level">
+            <div className="level-item has-text-centered">
+              <div>
+                <i className='fa fa-spinner fa-pulse fa-4x is-centered' aria-hidden='true'></i>
+              </div>
+            </div>
+          </nav>
+          : this.state.allImages.map(image =>
+            <Image 
+              title={image.title}
+              src={image.media.m}
+              flickrLink={image.link}
+              key={image.date_taken}
+            />
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ImageList;
