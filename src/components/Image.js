74910f2456894ee57769c988b502444c2c8334b4
Gemma <gemmawvoakes@gmail.com>
2017-11-26 18:00:41 +0000

    Props received in [Image] component and rendering.

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-function Image() {
+function Image(props) {
   return (
-    <div className='Image tile is-parent'>
-      <div className='tile is-child box'>
-        <h1>I am an image!</h1>
+      <div className='box'>
+        <h1>{props.title}</h1>
+        <a href={props.flickrLink} target='_blank'>
+          <img src={props.src} alt='sunrise' />
+        </a>
         <a className='button is-warning'>Add to favourites</a>
       </div>
-    </div>
-
   )
 }
 export default Image;
