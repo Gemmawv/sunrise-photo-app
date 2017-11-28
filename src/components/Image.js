df783cb0a0803f005c2a7c4720e63be70cf71970
Gemma <gemmawvoakes@gmail.com>
2017-11-28 11:59:52 +0000

    [Image] renders images with correct 'Add to favourites' or 'Remove from favourites' button.

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,14 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../css/Image.css';
+import PropTypes from 'prop-types';
 
-function Image(props) {
-  return (
-      <div className='box'>
-        <h1>{props.title}</h1>
-        <a href={props.flickrLink} target='_blank'>
-          <img src={props.src} alt='sunrise' />
-        </a>
-        <a className='button is-warning'>Add to favourites</a>
-      </div>
-  )
+class Image extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFave: this.props.isFave
+    }
+    this.AddFaveStatus = this.AddFaveStatus.bind(this)
+    this.RemoveFaveStatus = this.RemoveFaveStatus.bind(this)
+  }
+
+  render() {
+    return (
+      this.state.isFave ?
+        <div className='box'>
+          <div className='Image-details'>
+            <div className='Image-title'>
+              <h1>{this.props.title}</h1>
+              <i className='fa fa-heart' aria-hidden='true'></i>
+            </div>
+            <a href={this.props.flickrLink} target='_blank'>
+              <img src={this.props.src} alt='sunrise' />
+            </a>
+          </div>
+          <div className='FavesButton'>
+            <button className='button is-danger' onClick={this.RemoveFaveStatus}>Remove from favourites</button>
+          </div>
+        </div>
+        :
+        <div className='box'>
+          <div className='Image details'>
+            <h1>{this.props.title}</h1>
+            <a href={this.props.flickrLink} target='_blank'>
+              <img src={this.props.src} alt='sunrise' />
+            </a>
+          </div>
+          <div className='FavesButton'>
+            <button className='button is-warning' onClick={this.AddFaveStatus}>Add to favourites</button>
+          </div>
+        </div>
+    )
+  }
+
+  AddFaveStatus() {
+    this.props.addToFaves(this.props.imageDetails)
+    this.setState({
+      isFave: true
+    })
+  }
+
+  RemoveFaveStatus() {
+    this.props.removeFromFaves(this.props.imageDetails, this.props.id)
+    this.setState({
+      isFave: false
+    })
+  }
 }
+
+Image.propTypes = {
+  title: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  flickrLink: PropTypes.string.isRequired,
+  addToFaves: PropTypes.func.isRequired,
+  removeFromFaves: PropTypes.func.isRequired
+};
+
 export default Image;
