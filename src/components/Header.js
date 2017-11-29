a16f831e2b1a6ad47d1e6efad020d6c9c183b027
Gemma <gemmawvoakes@gmail.com>
2017-11-29 13:04:21 +0000

    Amend [Header] title.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Header() {
   return (
     <header className="App-header">
-    <h1 className="App-title title">Sunrise photo app</h1>
-    <h2 className='App-subtitle'>Find and save your favourite sunrise photos</h2>
-  </header>
+      <h1 className="App-title title">Sunrise photo finder</h1>
+      <h2 className='App-subtitle'>Find and save your favourite sunrise photos</h2>
+    </header>
   );
 }
 
