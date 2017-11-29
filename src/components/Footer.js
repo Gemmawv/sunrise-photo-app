838904161fc5d3ce32e3e7d2def619572e57d6d6
Gemma <gemmawvoakes@gmail.com>
2017-11-29 22:48:25 +0000

    Add link to [Footer].

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
+/* eslint-disable react/jsx-no-target-blank */
+
 import React from 'react';
 
 function Footer() {
   return (
-    <div className="App-footer">
-      <h1>Sunrise photo finder by Gemma Voakes</h1>
+    <div className='app-footer'>
+      <p>
+        <strong>Sunrise photo finder by </strong>
+        <a href='https://github.com/Gemmawv' target='_blank'> Gemma Voakes
+        </a>
+      </p>
     </div>
   );
 }
