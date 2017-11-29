2d6a1559c7d733add70ea9e69c98c0be1f0a96d3
Gemma <gemmawvoakes@gmail.com>
2017-11-29 13:03:06 +0000

    Add CSS to [Footer]

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Footer() {
   return (
-    <h1>I am a footer!</h1>
+    <div className="App-footer">
+      <h1>Sunrise photo finder by Gemma Voakes</h1>
+    </div>
   );
 }
 export default Footer;
